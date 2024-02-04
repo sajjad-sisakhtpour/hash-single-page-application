a95e5cd9f5d7ed3d0e3433d8fa30fae235039abc
sajjad <sajjad.sisakhtpour@gmail.com>
2024-02-04 12:49:20 +0330

    for all and end

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+document.querySelectorAll("A").forEach((Element) => {
+  Element.style.color = "black";
+});
 window.addEventListener("hashchange", (e) => {
+  let data;
   switch (location.hash) {
     case "#home":
-      console.log("yes");
-      document.querySelector("main").style.backgroundColor = "red";
-      document.querySelector("h1").innerText = "homeeee";
-      document.querySelector("a").style.color = "yellow";
+      data = { title: "home", color: "red", href: "#home" };
+      render(data);
+      break;
+    case "#search":
+      data = { title: "search", color: "pink", href: "#search" };
+      render(data);
+      break;
+    case "#likes":
+      data = { title: "likes", color: "green", href: "#likes" };
+      render(data);
+      break;
+    case "#profile":
+      data = { title: "profile", color: "yellow", href: "#profile" };
+      render(data);
       break;
   }
 });
+
+function render(data) {
+  document.querySelector("main").style.backgroundColor = data.color;
+  document.querySelector("h1").innerText = data.title;
+  document.querySelectorAll("a").forEach((Element) => {
+    if (Element.href.includes(data.href)) {
+      Element.style.color = "red";
+    } else {
+      Element.style.color = "black";
+    }
+  });
+}
